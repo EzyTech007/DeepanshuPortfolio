7e03bce605ee7206b5ab4de81537b75df5bcefa0
deepanshusignity <deepanshu@signitysolutions.com>
2026-05-24 16:26:26 +0530

    fix site url for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,12 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ezytech007.github.io",
+  base: "/DeepanshuPortfolio",
+  server: {
+    host: true,  // or use '0.0.0.0'
+    port: 4321   // default port, change if needed
+  },
   vite: {
     plugins: [tailwindcss()],
   },
